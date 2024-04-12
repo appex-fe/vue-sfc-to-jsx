b5b675e394597d8f2c16c75224463aa9bb595036
alonesuperman <alonesuperman@163.com>
2024-04-12 17:44:49 +0800

    feat: 添加日志功能并引入winston库
    本次更新主要是为了增强项目的日志管理能力，我们引入了winston库作为日志服务，并对`.gitignore`进行了更新，添加了`logs`目录以保持工作区的整洁。
    
    此次变更旨在提高项目的维护性和开发效率，使得开发者能更方便地追踪和管理日志信息。通过使用winston，我们可以更灵活地配置日志级别、格式以及存储位置，进一步提升了日志系统的可用性和灵活性。

---
+++ src/style-block/demo.ts
@@ -0,0 +1,4 @@
+import { handleStyleBlock } from "./index";
+import path from "path";
+
+handleStyleBlock(["src/views/site/chartFlowstats/FlowStatsReport.vue"].map(str => path.resolve("/Users/zhuxl/workspace/orch-frontend", str)))
---
+++ src/utils/logger.ts
@@ -0,0 +1,68 @@
+import winston from "winston";
+import { LEVEL } from "triple-beam";
+import path from "node:path";
+
+const logDir = path.resolve(process.cwd(), "logs");
+
+// 创建自定义格式，包括时间戳、颜色编码和日志级别
+const createLogFormat = (flag: "console" | "file", onlyLevel?: string) => {
+  const logFormat = winston.format.combine(
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss"
+    }),
+    flag !== "file" &&process.stdout.isTTY ? winston.format.colorize({ all: true }) : winston.format.uncolorize(),
+    winston.format.printf(
+      info => {
+        const pattern = `${info.timestamp} [${info.level}]: ${info.message}`;
+        if (onlyLevel) {
+          // 上了色之后，level会被污染，所以需要从info[LEVEL]中取原始的level，这个看不懂就打一次断点自己看看
+          return (info[LEVEL] === onlyLevel || info.level === onlyLevel) ? pattern : "";
+        }
+        return pattern;
+      }
+    )
+  );
+  return logFormat;
+}
+
+export const logger = winston.createLogger({
+  level: "silly",
+  format: createLogFormat("console"),
+  transports: [
+    // 控制台输出配置
+    new winston.transports.Console({
+      // 控制台记录所有级别的日志
+      level: "silly",
+      format: createLogFormat("console"),
+    }),
+    // 错误日志文件输出配置
+    new winston.transports.File({
+      filename: "error.log",
+      dirname: logDir,
+      level: "error",
+      format: createLogFormat("file", "error"),
+      // 日志文件最大5MB
+      maxsize: 5120000,
+      // 最多保留5个日志文件
+      maxFiles: 5,
+    }),
+    // 警告日志文件输出配置（仅记录 "warn" 级别）
+    new winston.transports.File({
+      filename: "warn.log",
+      dirname: logDir,
+      level: "warn",
+      format: createLogFormat("file", "warn"),
+      maxsize: 5120000,
+      maxFiles: 5,
+    }),
+    // 所有级别的日志均输出到all.log文件
+    new winston.transports.File({
+      filename: "all.log",
+      dirname: logDir,
+      format: createLogFormat("file"),
+      // 日志文件最大20MB
+      maxsize: 20480000,
+      maxFiles: 5,
+    }),
+  ]
+});
